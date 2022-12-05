7fc771eb4842d84db51d8a62f5cd184627b8b3d2
Usuario <Usuario@CPD02-M01>
2022-12-05 13:29:44 -0300

    testes

--- AppReclama/AppReclama/Views/Feed.xaml.cs
+++ AppReclama/AppReclama/Views/Feed.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppReclama.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,10 @@ namespace AppReclama.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Feed : ContentPage
     {
+
         public Feed()
         {
+            BindingContext = new FeedViewModel();
             InitializeComponent();
             logo.Source = ImageSource.FromResource("AppReclama.Img.peixelogotipo.png");
         }
